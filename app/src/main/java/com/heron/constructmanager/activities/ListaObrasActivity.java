107bf541d701c603e70ed5ab87246e9cfac57f16
Joao <joaoheron@gmail.com>
2020-10-19 20:20:04 -0300

    Created basic add obra button to listview

--- app/src/main/java/com/heron/constructmanager/activities/ListaObrasActivity.java
+++ app/src/main/java/com/heron/constructmanager/activities/ListaObrasActivity.java
@@ -2,16 +2,31 @@ package com.heron.constructmanager.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.heron.constructmanager.R;
+import com.heron.constructmanager.forms.NewObraForm;
 
 public class ListaObrasActivity extends AppCompatActivity {
+
+    Button add_obra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_obras);
+
+        add_obra = findViewById(R.id.list_obras_add_button);
+        add_obra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ListaObrasActivity.this, NewObraForm.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
